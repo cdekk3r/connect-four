16aa22b004ee440f030e08314a3b245c6e40eafe
Corey <cdekk3r@hotmail.com>
2017-12-07 21:55:51 +0000

    diagonal test

--- app.js
+++ app.js
@@ -4,41 +4,70 @@ var player1 = true;
 var count = 1;
 var lastSlot = 0;
 var temp = 0;
-var board = {};
+var table = [[0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0]];
 
-
-$('[data-row="1"]').on('click', function() { 
+$('[data-column]').on('click', function() { 
     
     var columnNum = $(this).attr('data-column');
-    var rowNum = $(this).attr('data-row');
     var column = $('[data-column="' + columnNum + '"]').toArray();
-    var length = column.length-1;
-    
         
     for(var i = column.length-1; i >= 0; i--) {
         var atRow = column[i];
+        var rowNum = $(atRow).attr('data-row');
         
         if ($(atRow).css('background-color')=="rgb(255, 255, 255)") {
         
             if(player1) {
                 $(atRow).css('background-color', 'black');
                 $(atRow).attr('data-player', '1');
+                table[rowNum-1][columnNum-1] = 1;
             } else {
                 $(atRow).css('background-color', 'red');
                 $(atRow).attr('data-player', '2');
+                table[rowNum-1][columnNum-1] = 2;
             }
             
-            player1 = !player1;
+            player1 = !player1; 
             break;
         } else {
             continue;
         }
     } 
-    
     checkHorizontalWin();
     checkVerticalWin();
+    checkDiagonalWin();
 });
+
+
+var checkDiagonalWin = function() {
+    // from bottom left up
+    for (var y = 5; y > 2; y--) {
+        for (var x = 0; x <= 3; x++) {
+            var player = player1 ? 2 : 1;
+            
+            if (table[y][x] == player && table[y-1][x+1] == player && table[y-2][x+2] == player && table[y-3][x+3] == player) {
+                alert("Win");
+            }
+        };
+    };
     
+    //from top left down
+    for (var y = 0; y < 3; y++) {
+        for (var x = 0; x < 5; x++) {
+            var player = player1 ? 2 : 1;
+            
+            if (table[y][x] == player && table[y+1][x+1] == player && table[y+2][x+2] == player && table[y+3][x+3] == player) {
+                alert("Win");
+            }
+        };
+    };
+};
+
 
 var checkHorizontalWin = function() {
     
@@ -109,8 +138,3 @@ var checkVerticalWin = function() {
     }
 };
 
-var checkDiagonalWin = function() {
-    
-    
-    
-};
